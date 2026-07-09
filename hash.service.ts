6d06c255361d52dbd14444be81809f5a72d9e15d
Shubham Tak <100914619+shub197@users.noreply.github.com>
2026-07-09 12:14:40 +0530

    hash service added

---
+++ hash.service.ts
@@ -0,0 +1,23 @@
+import bcrypt from 'bcrypt';
+
+async function generateHash(content: string) {
+    if (!content) {
+        throw new Error('No content.')
+    }
+
+    const hashed = await bcrypt.hash('working', 10);
+    return hashed;
+}
+
+async function compareHashed(content: string, hashed: string) {
+    if (!content) {
+        throw new Error('No content.')
+    }
+
+    const matched = await bcrypt.compare(content, hashed);
+    return matched;
+}
+
+const HashService = { generateHash, compareHashed }
+
+export default HashService;
